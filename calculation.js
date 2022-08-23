aba6ff43f2f271c7fe5825eb4ed01702237f651f
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-08-23 05:02:16 -0700

    top player 5 added

--- calculation.js
+++ calculation.js
@@ -1,15 +1,33 @@
 const playerArray = [];
+
+function display(name) {
+    const playerTable = document.getElementById('player-table');
+    playerTable.innerHTML = "";
+    for (let i = 0; i < name.length; i++) {
+        playerArray[i];
+        const singlePlayer = playerArray[i].name;
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${singlePlayer}</td>`;
+        playerTable.appendChild(tr);
+        console.log();
+
+    }
+}
 function addToTable(element) {
     const playerName = element.parentNode.children[0].innerText;
-    // elementValue = elementChildren.innerTax;
+    const playerNameObj = {
+        name: playerName
+    }
 
-    // playerArray.push(playerName);
-    // console.log(playerArray.length);
     if (playerArray.length >= 5) {
         alert('this is over');
         return;
 
     }
-    playerArray.push(playerName);
-    console.log(playerArray.length);
+    element.disabled = true;
+    playerArray.push(playerNameObj);
+
+    display(playerArray)
 }
